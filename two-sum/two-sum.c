162a4678a416e89eff6bdb7c7ab4ff26d0a8fb67
openistor <39221552+openistor@users.noreply.github.com>
2018-12-25 10:09:46 +0800

    Create two-sum.c

---
+++ two-sum/two-sum.c
@@ -0,0 +1,22 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target) {
+    
+    int i, j;
+    int *ret;
+    
+    ret = malloc(2*sizeof(int));
+    for (i=0;i<numsSize-1;i++) {
+        for (j=i+1; j < numsSize; j++) { 
+            if ((*(nums+i) + *(nums+j)) == target ) {
+                *ret = i;
+                *(ret+1) = j;
+                return ret;
+            }
+                        
+        }
+    }
+    return 0;
+    
+}
